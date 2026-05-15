424a822de56d96f53175fd4669535321a222b3b4
Willem Helf <sophie.helf@gmail.com>
2026-05-14 23:15:06 -0400

    added file import functionality and comments

--- nltk_text.py
+++ nltk_text.py
@@ -8,11 +8,16 @@ from nltk import FreqDist
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 import string
+from pathlib import Path
 
 ###
 
-file = 'dada.txt'
-file_text = open(file).read()
+def process_file(file_path):
+    print(f"Processing: {file_path}")
+    with open(file_path, 'r') as f:
+        data = f.read()
+        printout_test(data)
+
 
 '''To prepare text for analysis, we remove punctuation and 
 put all words in lowercase. We then break it down into a set
@@ -31,20 +36,32 @@ def prepare_text(text):
 
 def remove_stopwords(text):
     stripped_set = set()
-    stop_words = set(stopwords.text("english"))
+    stop_words = set(stopwords.words("english"))
     for word in text:
       if word not in stop_words:
         stripped_set.add(word)
     return stripped_set
 
-'''main function – return us the ten most common words'''
 
-if __name__ == '__main__':
-    prepared_text = prepare_text(file_text)
-    stripped_set = set()
-    stop_words = set(stopwords.words("english"))
-    for word in prepared_text:
-      if word not in stop_words:
-        stripped_set.add(word)
-    distribution = FreqDist(stripped_set)
-    print(distribution)
+'''find the top ten words used in the text'''
+
+def top_ten(words):
+   frequency_distribution = FreqDist(words)
+   return frequency_distribution.most_common(10)
+
+
+'''print out top ten words to the console'''
+
+def print_top_ten(text):
+   prepared = prepare_text(text)
+   no_stopwords = remove_stopwords(prepared)
+   top_ten_words = top_ten(no_stopwords)
+   print(top_ten_words)
+
+
+'''run file'''
+
+directory = Path('texts')
+
+for file in directory.glob('*.txt'):
+    process_file(file)
